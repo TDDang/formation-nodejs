de180cbf671b9689e8cb751b3edadd5d9da8e1c4
Thông-Duy <thongduy.dang@gmail.com>
2023-03-01 10:04:07 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('coucou les amis')
+console.log('Quelle est la destination des videos ?')
+
+// On affiche la variable d'environement nommé NODE_ENV
+
+console.log(process.env.NODE_ENV)
+
+// On affiche la variable d'environement nommé TZ
+
+console.log(process.env.TZ)
+
+// On affiche la variable d'environement nommé HOST
+
+console.log(process.env.HOST)
+
+// On affiche la variable d'environement nommé PORT
+
+console.log(process.env.PORT)
